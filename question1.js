b5a4ce965719d406aab5821ebb003f8c66b19ee2
Tony <tony.navarro@gmail.com>
2015-09-09 18:54:47 -0700

    adds comment blocks to problem 1 to explain each step of the logic.

--- question1.js
+++ question1.js
@@ -5,10 +5,10 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-var total = 0;
-for (var i = 0; i < 1000; i++) {
-  if (i % 3 === 0 | i % 5 === 0) {
-  total = total + i;
+var total = 0;                          // set my total to zero
+for (var i = 0; i < 1000; i++) {        // begin a for loop that will iterate 1000 times as per directions
+  if (i % 3 === 0 | i % 5 === 0) {      // if i is divisible by 3 or 5...
+  total = total + i;                    // we add it to the total
   };
 };
 
