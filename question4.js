7295e76b3a18dcdb3a791da2b6c0536eec49f089
Tony <tony.navarro@gmail.com>
2016-03-23 12:43:11 -0700

    Cleans up superfluous code. Slightly increases performance.

--- question4.js
+++ question4.js
@@ -1,60 +1,34 @@
 // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 //
 // Find the largest palindrome made from the product of two 3-digit numbers.
-/*
 
-Notes:
-xxx * yyy = palendromic number
-999 * 999 = 998001 <- highest possible number.
-Could we do a sort of reverse prime factor?
-
-(apparent) Rules:
-
-*/
-
-// for ( var i = 0; i <= 998001; i++ ) {
-//
-// }
 var x = 0;
 var y = 100;
 var z = 0;
 var digits = [];
 var palendromicNumbers = [];
-console.time("loop");
-while (z <= 998001) {
-  for ( var i = 100; i <= 999; i++ ) {
-    x = i;
-    z = x * y;
-    var digits = (""+z).split("");
-    var middle = digits[Math.floor(digits.length / 2)];
-    if ( digits.length === 6 && z % 2 === 0 ) {
-      if ( digits[0] === digits[digits.length -1] && digits[1] === digits[digits.length -2] && digits[2] === digits[digits.length -3]) {
-        palendromicNumbers.push({ x:x, y:y, z:z });
+var zAnswers = [];
+var highValue = 0;
+
+function palendromeNumber() {
+
+var begin = Date.now();
+
+  while (z <= 998001) {
+    for ( var i = 100; i <= 999; i++ ) {
+      x = i;
+      z = x * y;
+      var digits = (""+z).split("");
+
+      if ( digits.length === 6 ) {
+        if ( digits[0] === digits[digits.length -1] && digits[1] === digits[digits.length -2] && digits[2] === digits[digits.length -3]) {
+          palendromicNumbers.push({ x:x, y:y, z:z });
+          zAnswers.push(z);
+        }
       }
     }
+    y += 1;
   }
-  y += 1;
+  highValue = Math.max.apply( Math, zAnswers );
+  return highValue;
 }
-debugger;
-Array.max = function( palendromicNumbers ){
-    return Math.max.apply( Math, palendromicNumbers );
-};
-console.timeEnd("loop");
-  // x = i;
-  // y = i; // having will only do calculations like 111 * 111 need to have two loops.
-  // z = x * y;
-  // var digits = (""+z).split("");
-  // var middle = digits[Math.floor(digits.length / 2)];
-  //
-  // if ( digits.length === 6 && z % 2 === 0 ) {
-  //   // debugger;
-  //   if ( digits[0] === digits[digits.length -1] && digits[1] === digits[digits.length -2] && digits[2] === digits[digits.length -3]) {
-  //     console.log(z + "<- this should be palendromic");
-  //     alert(x + "::" + y);
-  //   } else {
-  //     console.log(z + "<- not palendromic");
-  //   }
-  // } else {
-  //   console.log(z + "<-- less than six digits");
-  // }
-// }
