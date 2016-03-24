04fed0dab49b4794aebc4198203fe805a9689b03
Tony <tony.navarro@gmail.com>
2016-03-24 14:27:10 -0700

    Puts this solution into a function and removes hardcoding when possible.

--- question4.js
+++ question4.js
@@ -2,29 +2,33 @@
 //
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-var x = 0;
-var y = 100;
-var z = 0;
-var digits = [];
-var palendromicNumbers = [];
-var zAnswers = [];
-var highValue = 0;
+function palendromeNumber(digitRange) {
+  var x = 0;
+  var z = 0;
+  var palendromicNumbers = [];
+  var zAnswers = [];
+  var highValue = 0;
+  var base = [1];
+  var ceiling = [9];
 
-function palendromeNumber() {
+  for (var k = 0; k < digitRange-1; k++) {
+    base.push(0);
+    ceiling.push(9);
+  }
+  base = parseInt(base.join(""));
+  ceiling = parseInt(ceiling.join(""));
 
-var begin = Date.now();
-
-  while (z <= 998001) {
-    for ( var i = 100; i <= 999; i++ ) {
+  var y = base
+  while (z < ceiling * ceiling) {
+    for ( var i = base; i <= ceiling; i++ ) {
       x = i;
       z = x * y;
-      var digits = (""+z).split("");
 
-      if ( digits.length === 6 ) {
-        if ( digits[0] === digits[digits.length -1] && digits[1] === digits[digits.length -2] && digits[2] === digits[digits.length -3]) {
-          palendromicNumbers.push({ x:x, y:y, z:z });
-          zAnswers.push(z);
-        }
+      var numStr = z.toString();
+      var revNumStr = numStr.split("").reverse().join("");
+      if ( numStr === revNumStr ) {
+        palendromicNumbers.push({ x:x, y:base, z:z });
+        zAnswers.push(z);
       }
     }
     y += 1;
